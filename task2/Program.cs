d92521b826a30248f9d58a605a143a0deb6d72b3
Daria Strekopytova <frau.strekopytova2010@yandex.ru>
2022-11-27 14:36:47 +0300

    2

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.*/
+
+Console.WriteLine("Введите число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N:");
+int numberN = int.Parse(Console.ReadLine());
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
